7a7b5462613cdf33a7c71faff2aa3e07937ed056
CarolinGrigor <125268417+CarolinGrigor@users.noreply.github.com>
2024-01-19 17:45:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    class Radio
+    {
+        public float Volume { get; set; }
+
+        public float AumentaVolume()
+        {
+            Volume += Volume * 0.1f;
+            return Volume;
+        }
+
+        public float DiminuisciVolume()
+        {
+            Volume -= Volume * 0.1f;
+            return Volume;
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Radio radio = new Radio();
+
+        radio.Volume = 50.0f;
+
+        Console.WriteLine($"Volume iniziale: {radio.Volume}");
+
+        radio.AumentaVolume();
+        Console.WriteLine($"Volume aumentato: {radio.Volume}");
+
+        radio.DiminuisciVolume();
+        Console.WriteLine($"Volume diminuito: {radio.Volume}");
+
+        Console.ReadKey();
+    }
+}
